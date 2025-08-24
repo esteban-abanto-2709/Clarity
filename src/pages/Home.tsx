cb6058094c1211834d0ef59291898064e87c54e8
Esteban Abanto <esteban.abanto.2709@gmail.com>
2025-08-24 14:23:23 -0500

    Feat(Styling): Integrate Tailwind CSS for styling, remove App.css, and update Home component with Tailwind classes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <h1>Bienvenido a la Whiteboard 🚀</h1>;
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+      <h1 className="text-4xl font-bold text-white underline">
+        Bienvenido a la Whiteboard 🚀
+      </h1>
+    </div>
+  );
 }
